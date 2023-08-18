ba29834c2ddff216a1ec37f9c73798abbe1fdbce
Felipe Araujo <zenycsoficial@gmail.com>
2023-08-17 21:15:49 -0300

    feats: new files

--- db.py
+++ db.py
@@ -14,10 +14,13 @@ db = firestore.client()
 # dados = {'CPF':12345678910, 'SALDO': 12000, 'FATURA': 200}
 # db.collection('contas').add(dados)
 
-doc_ref = db.collection("users").document("felaraujo")
+doc_ref = db.collection("users").document("11111111111")
 
 doc = doc_ref.get()
 if doc.exists:
-    print(f"Document data: {doc.to_dict()}")
+    print(f"Dados do usuario: {doc.to_dict()}")
+    conta = db.collection("contas").document("11111111111").get()
+    print(f"Dados da conta: {conta.to_dict()}")
+
 else:
     print("No such document!")
