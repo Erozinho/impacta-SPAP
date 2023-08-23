029122bd2774b5ac76a2b07d24193fe45a9c9535
Felipe Araujo <zenycsoficial@gmail.com>
2023-08-23 16:26:37 -0300

    🌐refactor: pequenas mudanças dentro do init

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask
+from website.page import page
 
 def create_app():
     app = Flask(__name__)
-    app.config ['SECRET KEY'] = 'BANCO123'
-
-    from website.page import page
+    app.secret_key = "BANCO123"
 
     app.register_blueprint(page, url_prefix="/")
-
 
     return app
