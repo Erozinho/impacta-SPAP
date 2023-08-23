c753104e4cbc6773bd19be0165e912c64395c2e9
Felipe Araujo <zenycsoficial@gmail.com>
2023-08-23 15:45:23 -0300

    ✨feat: Ultilizando session e flash message

--- website/page.py
+++ website/page.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template, session, redirect
+from flask import Blueprint, request, render_template, session, redirect, flash
 from db import db
 
 page = Blueprint('login', __name__, template_folder="template")
@@ -18,11 +18,20 @@ def register():
     nome = request.form.get('nome')
     cpf = request.form.get('cpf')
     senha = request.form.get('senha')
+
     dados = {"nome":nome, "cpf":cpf, "senha":senha}
+
+    doc_ref = db.collection('users').document(cpf)
+    doc = doc_ref.get()
+
+    if doc.exists:
+        flash("Já existe uma conta criado com esse CPF!", "info")
+        return ("register.html")
+    
     db.collection('users').document(cpf).set(dados)
     db.collection('contas').document(cpf).set({"saldo": 0, "fatura":0})
-    msg = "REGISTRADO COM SUCESSO"
-    return render_template("register.html", error=msg)
+    flash("Conta cadastrada com sucesso!", "info")
+    return render_template("login.html")
 
 
 @page.route('/login', methods=['GET'])
@@ -34,12 +43,20 @@ def carregar_l():
 def login():
     cpf = request.form.get('cpf')
     senha = request.form.get('senha')
-    print(cpf, senha)
+
     cred = db.collection("users").document(cpf).get()
     cred = (cred.to_dict())
+
     if cred['senha'] == senha:
         print('PASSOU')
-        return '<h1>SENHA CORRETA</h1>'
+        infos = db.collection("contas").document(cpf).get()
+        infos = infos.to_dict()
+        session["cpf"] = cred['cpf']
+        session["nome"] = cred['nome']
+        session["saldo"] = infos['saldo']
+        session["fatura"] = infos['fatura']
+        flash("Acesso bem sucedido!", "info")
+        return redirect("http://127.0.0.1:5000/home", code=302)
     else:
-        msg = "ALGUMA INFORMAÇÃO ESTA INCORRETA!"
-        return render_template('login.html', error=msg)
+        flash("INFORMAÇÕES INCORRETAS", "info")
+        return render_template('login.html')
