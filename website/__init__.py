4cece2840a896e3631f9a96a7bdf85edf4cd13f3
Felipe Araujo <zenycsoficial@gmail.com>
2023-10-29 02:52:12 -0300

    🥱feat and refactor: a lot changes on everything

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from website.page import page
+import os
+
+user = os.path.expanduser('~')
+dir_root = user + '\\Downloads'
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = "BANCO123"
+    app.config['UPLOAD_FOLDER'] = dir_root
+    app.config['MAX_CONTENT_LENGTH'] = 10000000
+    # app.config["SESSION_COOKIE_SAMESITE"] = "None"
+
 
     app.register_blueprint(page, url_prefix="/")
 
--- website/page.py
+++ website/page.py
@@ -1,11 +1,20 @@
-from flask import Blueprint, request, render_template, session, redirect, flash
+from flask import Blueprint, request, render_template, session, redirect, flash, url_for
 from db import db
+import pyimgur
+import requests, json
+from werkzeug.utils import secure_filename
+import os
+
+user = os.path.expanduser('~')
+dir_root = user + '\\Downloads'
+
+clientid = "ede3ed94c337f67"
 
 page = Blueprint('login', __name__, template_folder="template")
 
 @page.route('/')
 def home():
-    return redirect('http://127.0.0.1:5000/login', code=302)
+    return redirect("/login", code=302)
 
 
 @page.route('/register', methods=['GET'])
@@ -17,21 +26,22 @@ def carregar():
 def register():
     nome = request.form.get('nome')
     cpf = request.form.get('cpf')
+    email = request.form.get('email')
     cpf = cpf.replace(".","").replace("-","")
     senha = request.form.get('senha')
-    dados = {"nome":nome, "cpf":cpf, "senha":senha}
+    dados = {"nome":nome, "cpf":cpf, "senha":senha, "email":email, "pfp":"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"}
 
     doc_ref = db.collection('users').document(cpf)
     doc = doc_ref.get()
 
     if doc.exists:
         flash("Já existe uma conta criado com esse CPF!", "info")
-        return render_template('http://127.0.0.1:5000/register',code=302)
+        return render_template('/register',code=302)
     
     db.collection('users').document(cpf).set(dados)
     db.collection('contas').document(cpf).set({"saldo": 0, "fatura":0})
     flash("Conta cadastrada com sucesso!", "info")
-    return redirect('http://127.0.0.1:5000/login',code=302)
+    return redirect('/login',code=302)
 
 
 @page.route('/login', methods=['GET'])
@@ -52,10 +62,13 @@ def login():
         infos = infos.to_dict()
         session["cpf"] = sec['cpf']
         session["nome"] = sec['nome']
+        session["email"] = sec['email']
+        session["pfp"] = sec['pfp']
+        session["senha"] = sec['senha']
         session["saldo"] = infos['saldo']
         session["fatura"] = infos['fatura']
         flash("Acesso bem sucedido!", "info")
-        return redirect('http://127.0.0.1:5000/home',code=302)
+        return redirect('/home',code=302)
 
     flash("INFORMAÇÕES INCORRETAS", "error")
     return render_template('login.html')
@@ -66,9 +79,10 @@ def logout():
     session.pop('cpf', None)
     session.pop('nome', None)
     session.pop('saldo', None)
+    session.pop('email', None)
     session.pop('fatura', None)
     flash("Saida bem sucedida!", "info")
-    return redirect('http://127.0.0.1:5000/login',code=302)
+    return redirect('/login',code=302)
 
 
 @page.route('/home', methods=['GET'])
@@ -76,7 +90,7 @@ def carregar_h():
     if 'cpf' in session:
         return render_template('home.html')
     flash("Você não esta logado!", "warning")
-    return redirect("http://127.0.0.1:5000/login", code=302)
+    return redirect("/login", code=302)
 
 
 @page.route('/transfer', methods=['GET'])
@@ -86,7 +100,7 @@ def carregar_t():
         return render_template('transfer.html')
 
     flash("Você não esta logado!", "warning")
-    return redirect("http://127.0.0.1:5000/login", code=302)
+    return redirect("/login", code=302)
 
 
 @page.route('/transfer', methods=['POST'])
@@ -107,7 +121,7 @@ def transferir():
 
         if valor_total < valor:
             flash("Saldo insucificente", "warning")
-            return redirect("http://127.0.0.1:5000/transfer", code=302) 
+            return redirect("/transfer", code=302) 
             
         db.collection("contas").document(ca_info['cpf']).update({"saldo": ca_conta['saldo']+valor})
         db.collection("contas").document(session['cpf']).update({"saldo": float(session['saldo']) - valor})
@@ -115,10 +129,10 @@ def transferir():
         session["saldo"] = valor_total - valor
             
         flash("Transferencia realizada com sucesso!", "warning")
-        return redirect("http://127.0.0.1:5000/home", code=302)
+        return redirect("/home", code=302)
 
     flash("CPF invalido/ Não Cadastrado", "warning")
-    return redirect("http://127.0.0.1:5000/transfer", code=302)
+    return redirect("/transfer", code=302)
 
 
 @page.route('/fatura', methods=['GET'])
@@ -128,7 +142,7 @@ def carregar_f():
         return render_template('fatura.html')
 
     flash("Você não esta logado!", "warning")
-    return redirect("http://127.0.0.1:5000/login", code=302)
+    return redirect("/login", code=302)
 
 
 @page.route('/fatura', methods=['POST'])
@@ -140,20 +154,47 @@ def fatura():
 
     if saldo < valor:
         flash("Saldo insucificente", "warning")
-        return redirect("http://127.0.0.1:5000/fatura", code=302) 
+        return redirect("/fatura", code=302) 
 
     if (fatura - valor) <= 0:
         flash("Favor informar um valor igual ou menor que sua fatura!", "warning")
-        return redirect("http://127.0.0.1:5000/home", code=302)
+        return redirect("/home", code=302)
     
     db.collection("contas").document(session['cpf']).update({"fatura": float(session['fatura']) - valor})
     db.collection("contas").document(session['cpf']).update({"saldo": float(session['saldo']) - valor})
     session['fatura'] = fatura - valor
     session['saldo'] = saldo - valor
     flash("Fatura paga com sucesso!", "warning")
-    return redirect("http://127.0.0.1:5000/home", code=302)
+    return redirect("/home", code=302)
 
 
 @page.route('/profile', methods=['GET'])
 def profile_c():
-    return render_template('profile.html')
+    return render_template('profile.html',)
+
+
+@page.route('/profile', methods=['POST'])
+def profile_e():
+
+    # pega os edit do primo
+    nome = request.form.get('nome')
+    file = request.files['file']
+    file.save((os.path.join(dir_root, secure_filename(file.filename))))
+    file = os.path.join(dir_root, secure_filename(file.filename))
+
+    # upando img e apagando temp
+    im = pyimgur.Imgur(clientid)
+    upimg = im.upload_image(file)
+    os.remove(file)
+
+    # att o banco e instancia no session dnv
+    db.collection("users").document(session['cpf']).update({"pfp": upimg.link})
+    cred = db.collection("users").document(session['cpf']).get()
+    sec = (cred.to_dict())
+    session["nome"] = sec['nome']
+    session["email"] = sec['email']
+    session["pfp"] = sec['pfp']
+    session["senha"] = sec['senha']
+    return redirect("/profile", code=302)
+
+
